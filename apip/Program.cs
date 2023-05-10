474ed056fb849f857af7f59e91467f6e3fe3e16a
Dennis_Mugarte <107282802+DennisMugarte@users.noreply.github.com>
2023-05-09 19:53:06 -0600

    Habilitar Cors
    Se añadio una habilitacion de Cors para agregar todo los methodos

--- apip/Program.cs
+++ apip/Program.cs
@@ -3,6 +3,15 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ProductoDb>(opt => opt.UseInMemoryDatabase("ProductoList"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddCors(options => {
+    //* Se configura una politica
+    options.AddPolicy("Nueva Politica", app => {
+        //* Permite agregar todo los origenes, cabezera y methodos(GET, POST, PUT Y DELETE)
+        app.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 //Con la siguiente url pueden acceder a la pagina, por pagina se obtiene 5 productos http://localhost:DEPENDIENDO SU PUERTO/productos?page=2&pageSize=5 By Stephanie uwu
@@ -88,5 +97,8 @@ app.MapDelete("/productos/{id}", async (int id, ProductoDb db) => {
     }
     return Results.NotFound();
 });
+
+//* Permite la habilitacion de Cors
+app.UseCors("Nueva Politica");
 
 app.Run();
